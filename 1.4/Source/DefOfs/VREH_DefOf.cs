22ca5e7291a4a59944b9a277c6a08057c1d64328
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-11-25 15:09:10 +0100

    Tweaked luci dependency

--- 1.4/Source/DefOfs/VREH_DefOf.cs
+++ 1.4/Source/DefOfs/VREH_DefOf.cs
@@ -9,5 +9,6 @@ namespace VREHussars
         public static GeneDef VREH_BulletproofSkin;
         public static GeneDef VREH_Giant;
         public static GeneDef VREH_Unconstrained;
+        public static HediffDef VREH_GeneticDrugNeed_Luci;
     }
 }
---
+++ 1.4/Source/Genes/Gene_ChemicalDependencyLuci.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+using VREHussars;
+
+namespace VREHussars
+{
+    public class Gene_ChemicalDependencyLuci : Gene_ChemicalDependency
+    {
+
+
+        public override void PostAdd()
+        {
+            if (!ModLister.CheckBiotech("Chemical dependency"))
+            {
+                return;
+            }
+            base.PostAdd();
+            if (def.chemical.addictionHediff != null)
+            {
+                Hediff firstHediffOfDef = pawn.health.hediffSet.GetFirstHediffOfDef(def.chemical.addictionHediff);
+                if (firstHediffOfDef != null)
+                {
+                    pawn.health.RemoveHediff(firstHediffOfDef);
+                }
+            }
+            Hediff_ChemicalDependency hediff_ChemicalDependency = (Hediff_ChemicalDependency)HediffMaker.MakeHediff(VREH_DefOf.VREH_GeneticDrugNeed_Luci, pawn);
+            hediff_ChemicalDependency.chemical = def.chemical;
+            pawn.health.AddHediff(hediff_ChemicalDependency);
+            lastIngestedTick = Find.TickManager.TicksGame;
+        }
+
+    }
+}
