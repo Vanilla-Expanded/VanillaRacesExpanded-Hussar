4a0797b211d15d183bcea7a0ec160cd63c778e05
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-07-13 10:38:14 +0200

    Fixed Luciferium need hediff description

---
+++ 1.4/Source/Hediff/Hediff_ChemicalDependency_Luci.cs
@@ -0,0 +1,31 @@
+﻿
+using RimWorld;
+using Verse;
+
+namespace VREHussars
+{
+    public class Hediff_ChemicalDependency_Luci : Hediff_ChemicalDependency
+    {
+
+
+        public override string TipStringExtra
+        {
+            get
+            {
+                string text = "";
+                Gene_ChemicalDependency linkedGene = LinkedGene;
+                if (linkedGene != null)
+                {
+                    if (!text.NullOrEmpty())
+                    {
+                        text += "\n\n";
+                    }
+                    text += "GeneDefChemicalNeedDurationDesc".Translate(chemical.label, pawn.Named("PAWN"), "PeriodDays".Translate(10f).Named("DEFICIENCYDURATION"), "PeriodDays".Translate(20f).Named("COMADURATION"), "PeriodDays".Translate(30f).Named("DEATHDURATION")).Resolve();
+                    text = text + "\n\n" + "LastIngestedDurationAgo".Translate(chemical.Named("CHEMICAL"), (Find.TickManager.TicksGame - linkedGene.lastIngestedTick).ToStringTicksToPeriod().Named("DURATION")).Resolve();
+                }
+                return text;
+            }
+        }
+
+    }
+}
